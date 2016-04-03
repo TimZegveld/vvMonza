f742527c33f52475aaf006d39a3330ef6f9c8932
Tim Zegveld <tim.zegveld@gmail.com>
2016-04-03 13:30:12 +0200

    Startup

---
+++ vvMonza/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vvMonza.Startup))]
+namespace vvMonza
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
